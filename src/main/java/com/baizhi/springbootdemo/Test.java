f7d2b6b570b30a82b564d9811f1e2319939ede8b
yjs <1124873737@qq.com>
2018-11-26 09:18:26 +0800

    version 1.0.1

---
+++ src/main/java/com/baizhi/springbootdemo/Test.java
@@ -0,0 +1,10 @@
+package com.baizhi.springbootdemo;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class Test {
+    public void m2() {
+        System.out.println("111111");
+    }
+}
