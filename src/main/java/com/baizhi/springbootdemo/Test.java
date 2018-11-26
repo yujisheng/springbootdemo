c8c4cd376c8fc5c0ad3fbff0c6007522ec384e68
yjs <1124873737@qq.com>
2018-11-26 23:06:16 +0800

    version 1.0.1

--- src/main/java/com/baizhi/springbootdemo/Test.java
+++ src/main/java/com/baizhi/springbootdemo/Test.java
@@ -7,6 +7,8 @@ public class Test {
     public void m2() {
         System.out.println("11111123123");
         System.out.println("美丽的神话");
-        Syotem.out.println("天下第一");
+        System.out.println("天下第一");
+        System.out.println("天下第一");
+        System.out.println("天下第一");
     }
 }
