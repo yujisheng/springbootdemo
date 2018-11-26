6146ed7fe26661f8d4412b0ed7dcbf314bb51a3d
yjs <1124873737@qq.com>
2018-11-26 09:20:12 +0800

    version 1.0.0

--- src/main/java/com/baizhi/springbootdemo/Test.java
+++ src/main/java/com/baizhi/springbootdemo/Test.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class Test {
     public void m2() {
-        System.out.println("111111");
+        System.out.println("111111231");
     }
 }
